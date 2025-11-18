d2daa31444e121a697972a2b84c510a7b5fa79cf
andrianxyz82-hue <andrianxyz82@gmail.com>
2025-11-19 02:11:59 +0700

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,74 @@
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+import 'package:best_flutter_ui_templates/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return FitnessAppHomeScreen();
+  }
+}
+
+class HomeListView extends StatelessWidget {
+  const HomeListView(
+      {Key? key,
+      this.listData,
+      this.callBack,
+      this.animationController,
+      this.animation})
+      : super(key: key);
+
+  final HomeList? listData;
+  final VoidCallback? callBack;
+  final AnimationController? animationController;
+  final Animation<double>? animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController!,
+      builder: (BuildContext context, Widget? child) {
+        return FadeTransition(
+          opacity: animation!,
+          child: Transform(
+            transform: Matrix4.translationValues(
+                0.0, 50 * (1.0 - animation!.value), 0.0),
+            child: AspectRatio(
+              aspectRatio: 1.5,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                child: Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Image.asset(
+                        listData!.imagePath,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        splashColor: Colors.grey.withOpacity(0.2),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(4.0)),
+                        onTap: callBack,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/navigation_home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:best_flutter_ui_templates/app_theme.dart';
+import 'package:best_flutter_ui_templates/custom_drawer/drawer_user_controller.dart';
+import 'package:best_flutter_ui_templates/custom_drawer/home_drawer.dart';
+import 'package:best_flutter_ui_templates/feedback_screen.dart';
+import 'package:best_flutter_ui_templates/help_screen.dart';
+import 'package:best_flutter_ui_templates/home_screen.dart';
+import 'package:best_flutter_ui_templates/invite_friend_screen.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+import 'package:best_flutter_ui_templates/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.white,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: FitnessAppHomeScreen(),
+        ),
+      ),
+    );
+  }
+}
